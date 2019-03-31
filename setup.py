6c5fb1a3e6e0199c04f2e036cf7a6193979ca7a0
Yachna Sharma <yachnasharma@Yachnas-MacBook-Air.local>
2019-03-31 16:56:41 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from glob import glob
+
+
+setup(
+        name='VIntel',
+        version='0.1',
+        packages=find_packages(),
+        include_package_data=True
+    )
