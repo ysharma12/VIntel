a9d5ff26526d578bd45b44212e54ecb8d87b0c8d
Yachna Sharma <yachnasharma@Yachnas-MacBook-Air.local>
2019-03-31 17:29:35 -0400

    minor updates

--- data_processing/processing.py
+++ data_processing/processing.py
@@ -1,9 +1,9 @@
 """This script defines class and methods for image data processing"""
 
-import os, sys
-import shutil
-from os.path import dirname, abspath, basename, exists, splitext
+import os
+from os.path import exists, splitext
 from os.path import join as join_path
+import shutil
 from utilities import utils as ut
 from image_features import features
 
@@ -26,21 +26,21 @@ class DataProcess():
     def flatten_subdir(self, current_dir):
         """Move all files in subdirs to here, then delete subdirs.
            Conflicting files are renamed, with 1 appended to their name."""
-    
-        for root, dirs, files in os.walk(current_dir, topdown=False ):
+        for root, dirs, files in os.walk(current_dir, topdown=False):
             if root != current_dir:
                 for name in files:
                     source = join_path(root, name)
-                    target = self.handleDuplicates(join_path(current_dir, name))
+                    target = self.handle_duplicates(join_path(current_dir, name))
                     os.rename(source, target)
 
             for name in dirs:
                 os.rmdir(join_path(root, name))
-            
-    def handleDuplicates(self, target):
+
+    def handle_duplicates(self, target):
+        """Appends a marker to duplicate file names"""
         while exists(target):
             base, ext = splitext(target)
-            target    = base + self.duplicate_file_marker + ext
+            target = base + self.duplicate_file_marker + ext
         return target
 
     def read_and_process(self):
@@ -48,13 +48,13 @@ class DataProcess():
            and filters out the outlier images based on GLCM dissimilarity
            threshold for the given class.
         """
-        for cls, params in util.class_labels.items():
+        for cls, _ in util.class_labels.items():
             self.flatten_subdir(util.data_dir + '/' + cls + '/')
-            
-    def copy_file(self):
+
+    def copy_file(self, src, dest):
         """Copies file from source to destination"""
         src_files = os.listdir(src)
         for file_name in src_files:
             full_file_name = os.path.join(src, file_name)
-            if (os.path.isfile(full_file_name)):
+            if os.path.isfile(full_file_name):
                 shutil.copy(full_file_name, dest)
--- image_features/features.py
+++ image_features/features.py
@@ -1,11 +1,11 @@
-"""This script defines class and methods for feature computation and filtering out images not suitable for training"""
+"""This script defines class and methods for feature computation and
+   filtering out images not suitable for training"""
 
-import os, sys
-import cv2
+import os
 import shutil
-import numpy as np
 from glob import glob
-
+import cv2
+import numpy as np
 from skimage.feature import greycomatrix, greycoprops
 from utilities import utils as ut
 
@@ -15,9 +15,10 @@ HEIGHT = util.image_height
 
 class ImageFeatures():
     """Computes discrete fourier transform of an image to capture image frequencies.
-       The dft image is used to compute the image texture features using GLCM(gray level co-occurence matrices)
+       The dft image is used to compute the image texture features using
+       GLCM(gray level co-occurence matrices)
     """
-    
+
     def __init__(self):
         """Initializes imagefeatures"""
 #         self.image = image
@@ -42,11 +43,12 @@ class ImageFeatures():
         dft_shift = np.fft.fftshift(dft)
         magnitude_spectrum = np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
         return magnitude_spectrum
-    
+
     def get_glcm_features(self, input_image):
         """Computes glcm features for the give input image"""
         image = np.uint8(input_image)
-        glcm = greycomatrix(image, [5], list(np.arange(0, 360, 30)), 256, symmetric=True, normed=True)
+        glcm = greycomatrix(image, [5], list(np.arange(0, 360, 30)),
+                            256, symmetric=True, normed=True)
         dis = greycoprops(glcm, 'dissimilarity')[0][0]
         cor = greycoprops(glcm, 'correlation')[0][0]
         con = greycoprops(glcm, 'contrast')[0][0]
@@ -54,16 +56,16 @@ class ImageFeatures():
         asm = greycoprops(glcm, 'ASM')[0][0]
         energy = greycoprops(glcm, 'energy')[0][0]
         return {'dissimilarity': dis,
-            'correlation':cor,
-            'contrast':con,
-            'homogeneity': hom,
-            'asm': asm,
-            'energy': energy,
-            }
-
+                'correlation':cor,
+                'contrast':con,
+                'homogeneity': hom,
+                'asm': asm,
+                'energy': energy,
+               }
 
     def update_results(self, result, glcm_props, quality_label, image_name, class_label):
-        for k,_ in glcm_props.items():
+        """Updates the glcm properties with data for current image."""
+        for k, _ in glcm_props.items():
             if k == 'quality_labels':
                 glcm_props[k].append(quality_label)
             elif k == 'image_name':
@@ -77,31 +79,33 @@ class ImageFeatures():
 
     def filter_images(self, move_to_dir=False):
         """Reads images for each class and removes images not suitable for training."""
-        
+
         unread_files = []
-        for cls,_ in util.class_labels.items():
+        for cls, _ in util.class_labels.items():
             cls_path = util.data_dir + '/' + cls + '/'
             print(cls_path)
             images = glob(os.path.join(cls_path, "*.jpg"))
-            for i,img in enumerate(images):
+            for _, img in enumerate(images):
                 try:
                     image = cv2.imread(img)
                     image_filename = img.split('/')[-1]
-                    height, width, channels = image.shape
+                    height, width, _ = image.shape
 #                     print(height, width)
                     if height < 1000 or width < 1000:
                         label = 0
                     else:
                         label = 1
                     image = cv2.resize(image, (WIDTH, HEIGHT), interpolation=cv2.INTER_CUBIC)
-                    image = cv2.normalize(image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+                    image = cv2.normalize(image, None, alpha=0, beta=1,
+                                          norm_type=cv2.NORM_MINMAX,
+                                          dtype=cv2.CV_32F)
                     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                     magnitude_spectrum = self.fft_spectrum(gray)
                     result = self.get_glcm_features(magnitude_spectrum)
-                    self.glcm_props = self.update_results(result, self.glcm_props, label, image_filename, cls)
+                    self.glcm_props = self.update_results(result, self.glcm_props,
+                                                          label, image_filename, cls)
                     if result['dissimilarity'] > util.glcm_thresh[cls] and move_to_dir:
                         destination_dir = cls_path + '_filtered_out/'
-                        # move the image to filtered folder where images not suitable for training are placed
                         if not os.path.exists(destination_dir):
                             os.mkdir(destination_dir)
                         shutil.move(img, destination_dir + image_filename)
